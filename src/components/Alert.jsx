df7c3d85e9d7c037f0e1abe2d821dd21b074f905
SOG-web <raheemolalekanusman84@gmail.com>
2022-10-06 15:45:49 +0100

    job page fully done, document remains the verify and decline feature, user remains chat option

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,26 +1,32 @@
-export const Alert = ({ title, text, setIsPositive }) => {
+export const Alert = ({ title, text, setIsPositive, callBack }) => {
   return (
-    <div className="bg-[#F5F5F5] rounded-[20px] p-[34px] w-[90%] max-w-[439px] text-center">
+    <div className='bg-[#F5F5F5] rounded-[20px] p-[34px] w-[90%] max-w-[439px] text-center'>
       <div
         className={`${
-          title === "DELETE" ? "text-[#C90415]" : "text-[#000]"
+          title === 'DELETE' ? 'text-[#C90415]' : 'text-[#000]'
         } text-[16px] font-[700] leading-[20px] mb-[29px]`}
       >
         {title}
       </div>
-      <p className="text-[#000] text-[16px] font-[600] leading-[20px] mb-[52px]">
+      <p className='text-[#000] text-[16px] font-[600] leading-[20px] mb-[52px]'>
         {text}
       </p>
-      <div className="flex flex-col items-center gap-[7px]">
+      <div className='flex flex-col items-center gap-[7px]'>
         <button
-          onClick={() => setIsPositive("yes")}
-          className="text-[#303030] text-[12px] font-[700] leading-6 bg-[#E6E6E6] rounded-[19px] px-[86px] py-[4px] tracking-[-0.006em]"
+          onClick={() => {
+            setIsPositive('yes');
+            callBack('yes');
+          }}
+          className='text-[#303030] text-[12px] font-[700] leading-6 bg-[#E6E6E6] rounded-[19px] px-[86px] py-[4px] tracking-[-0.006em]'
         >
           YES
         </button>
         <button
-          onClick={() => setIsPositive("no")}
-          className="text-[#fff] text-[12px] font-[700] leading-6 bg-[#02378B] rounded-[19px] px-[86px] py-[4px] tracking-[-0.006em]"
+          onClick={() => {
+            setIsPositive('no');
+            callBack('no');
+          }}
+          className='text-[#fff] text-[12px] font-[700] leading-6 bg-[#02378B] rounded-[19px] px-[86px] py-[4px] tracking-[-0.006em]'
         >
           NO
         </button>
--- src/pages/JobseekersEmployeesDocument.jsx
+++ src/pages/JobseekersEmployeesDocument.jsx
@@ -3,12 +3,16 @@ import { useState, useEffect } from 'react';
 import decline from '../assets/decline.svg';
 import verify from '../assets/verify.svg';
 import file from '../assets/file.svg';
+import { Alert } from '../components/Alert';
 
 import { DataGrid } from '@mui/x-data-grid';
+import useAxios from '../hooks/useAxios';
+import { LoadingIndicator } from '../components/LoadingIndicator';
 
-export const JobseekersEmployeesDocument = ({ tableData }) => {
+export const JobseekersEmployeesDocument = ({ tableData, setRefresh }) => {
   const [isPositive, setIsPositive] = useState(null);
   const [showAlert, setShowAlert] = useState(false);
+  const [alertDetails, setAlertDetails] = useState();
   const [pageState, setPageState] = useState({
     isLoading: true,
     data: [],
@@ -26,90 +30,74 @@ export const JobseekersEmployeesDocument = ({ tableData }) => {
     }));
   }, [pageState.page, pageState.pageSize]);
 
+  const request = useAxios();
+
+  const {
+    makeRequest,
+    isLoading,
+    setErrorMessage,
+    errorMessage,
+    success,
+    data: response,
+  } = request();
+
+  const onCertDecide = (id, data, type) => {
+    if (type === 'verify') {
+      makeRequest({
+        url: `jobseekers/user-document-review/${id}/verify_credential/`,
+        method: 'PUT',
+        payload: data,
+      });
+    }
+
+    if (type === 'decline') {
+      makeRequest({
+        url: `jobseekers/user-document-review/${id}/decline_credential/`,
+        method: 'PUT',
+        payload: data,
+      });
+    }
+    setErrorMessage(errorMessage);
+    console.log(errorMessage);
+    return;
+  };
+
+  useEffect(() => {
+    if (success) {
+      console.log(response);
+      setRefresh(true);
+      return;
+    }
+  }, [response]);
+
   const columns = [
     { field: 'col1', headerName: 'id', width: 70 },
     { field: 'col2', headerName: 'name', width: 170 },
     {
       field: 'col3',
-      headerName: 'cv',
-      width: 170,
-      sortable: false,
-      renderCell: (params) => {
-        return (
-          <div className='flex flex-row gap-2'>
-            <div className='hover:cursor-pointer flex flex-row gap-2 items-center'>
-              <img src={file} alt='file-icon' width={20} height={20} />
-              <p className='w-8 text-[8px] h-3 text-clip text-center overflow-hidden'>
-                {params.value}
-              </p>
-            </div>
-            <div className='flex items-center gap-[11px] text-[10px] font-[700] leading-6'>
-              <button className='flex items-center gap-[5px] text-[#00944D]'>
-                <img src={verify} alt='chat-icon' />
-                <span>Verify</span>
-              </button>
-
-              <button
-                // onClick={onClick}
-                className='flex items-center gap-[5px] text-[#C90415]'
-              >
-                <img src={decline} alt='delete-icon' />
-                <span>Decline</span>
-              </button>
-            </div>
-          </div>
-        );
-      },
-    },
-    {
-      field: 'col4',
-      headerName: 'resume',
-      width: 170,
-      sortable: false,
-      renderCell: (params) => {
-        return (
-          <div className='flex flex-row gap-2'>
-            <div className='hover:cursor-pointer flex flex-row gap-2 items-center'>
-              <img src={file} alt='file-icon' width={20} height={20} />
-              <p className='w-8 text-[8px] h-3 text-clip text-center overflow-hidden'>
-                {params.value}
-              </p>
-            </div>
-            <div className='flex items-center gap-[11px] text-[10px] font-[700] leading-6'>
-              <button className='flex items-center gap-[5px] text-[#00944D]'>
-                <img src={verify} alt='chat-icon' />
-                <span>Verify</span>
-              </button>
-
-              <button
-                // onClick={onClick}
-                className='flex items-center gap-[5px] text-[#C90415]'
-              >
-                <img src={decline} alt='delete-icon' />
-                <span>Decline</span>
-              </button>
-            </div>
-          </div>
-        );
-      },
-    },
-    {
-      field: 'col5',
-      headerName: 'certificates',
+      headerName: 'cv/resume',
       width: 190,
       sortable: false,
-      renderCell: (params) => (
-        <ul className='flex flex-col gap-2 py-1'>
-          {params.value.map((cv, index) => (
-            <div key={index} className='flex flex-row gap-2 items-center'>
-              <a href={cv.file} target='blank'>
-                <div className='hover:cursor-pointer flex flex-row gap-2 items-center'>
-                  <img src={file} alt='file-icon' width={20} height={20} />
-                  <li className='w-8 text-[8px] h-3 text-clip text-center overflow-hidden'>
-                    {cv.name}
-                  </li>
-                </div>
-              </a>
+      renderCell: (params) => {
+        return (
+          <div className='flex flex-row gap-2'>
+            <div className='hover:cursor-pointer flex flex-row gap-2 items-center'>
+              {params.value === 'nothing to show' ? null : (
+                <img src={file} alt='file-icon' width={20} height={20} />
+              )}
+              {params.value === 'nothing to show' ? (
+                <p className='w-8 text-[8px] h-3 text-clip text-center overflow-hidden'>
+                  empty
+                </p>
+              ) : (
+                <a href={params.value?.file} target='blank'>
+                  <p className='w-8 text-[8px] h-3 text-clip text-center overflow-hidden'>
+                    {params.value?.file}
+                  </p>
+                </a>
+              )}
+            </div>
+            {params.value === 'nothing to show' ? null : (
               <div className='flex items-center gap-[11px] text-[10px] font-[700] leading-6'>
                 <button className='flex items-center gap-[5px] text-[#00944D]'>
                   <img src={verify} alt='chat-icon' />
@@ -124,6 +112,81 @@ export const JobseekersEmployeesDocument = ({ tableData }) => {
                   <span>Decline</span>
                 </button>
               </div>
+            )}
+          </div>
+        );
+      },
+    },
+    {
+      field: 'col4',
+      headerName: 'certificates',
+      width: 210,
+      sortable: false,
+      renderCell: (params) => (
+        <ul className='flex flex-col gap-2 py-1'>
+          {params.value.map((cv, index) => (
+            <div key={index} className='flex flex-row gap-2 items-center'>
+              <a href={cv.file} target='blank'>
+                <div className='hover:cursor-pointer flex flex-row gap-2 items-center'>
+                  <img src={file} alt='file-icon' width={20} height={20} />
+                  <li className='w-8 text-[8px] h-3 text-clip text-center overflow-hidden'>
+                    {cv.name}
+                  </li>
+                </div>
+              </a>
+              <div className='flex items-center gap-[11px] text-[10px] font-[700] leading-6'>
+                <button
+                  className='flex items-center gap-[5px] text-[#00944D]'
+                  onClick={() => {
+                    const data = {
+                      ...cv,
+                      reviewed: 'verified',
+                    };
+                    setAlertDetails({
+                      title: 'Verify Document',
+                      text: `Are you sure you want to verify ${cv.name}?`,
+                      data: { id: cv.id, data, type: 'verify' },
+                    });
+                    setShowAlert(true);
+                  }}
+                  disabled={cv.reviewed === 'Verified'}
+                >
+                  {cv.reviewed !== 'Verified' && (
+                    <img src={verify} alt='chat-icon' />
+                  )}
+                  {cv.reviewed !== 'Verified' ? (
+                    <span>Verify</span>
+                  ) : (
+                    <span>Verified</span>
+                  )}
+                </button>
+
+                <button
+                  className='flex items-center gap-[5px] text-[#C90415]'
+                  onClick={() => {
+                    const data = {
+                      ...cv,
+                      reviewed: 'declined',
+                    };
+                    setAlertDetails({
+                      title: 'Decline Document',
+                      text: `Are you sure you want to decline ${cv.name}?`,
+                      data: { id: cv.id, data, type: 'decline' },
+                    });
+                    setShowAlert(true);
+                  }}
+                  disabled={cv.reviewed === 'Declined'}
+                >
+                  {cv.reviewed !== 'Declined' && (
+                    <img src={decline} alt='delete-icon' />
+                  )}
+                  {cv.reviewed !== 'Declined' ? (
+                    <span>Decline</span>
+                  ) : (
+                    <span>Declined</span>
+                  )}
+                </button>
+              </div>
             </div>
           ))}
         </ul>
@@ -131,7 +194,7 @@ export const JobseekersEmployeesDocument = ({ tableData }) => {
       type: 'string',
     },
     {
-      field: 'col6',
+      field: 'col5',
       headerName: 'action',
       width: 120,
       sortable: false,
@@ -201,13 +264,34 @@ export const JobseekersEmployeesDocument = ({ tableData }) => {
         <div className='fixed top-[0] right-[0] bottom-[0] left-[255px] flex justify-center items-center bg-[#918a8a61]'>
           <div>
             <Alert
-              title='DELETE'
-              text={`Are you sure you want to delete ${
-                toDeleteDetail.name.trim() ? toDeleteDetail.name : 'this user'
-              }?, it can't be reversed?`}
+              title={alertDetails.title}
+              text={alertDetails.text}
               setIsPositive={setIsPositive}
+              callBack={(type) => {
+                console.log(type);
+                setShowAlert(false);
+                if (type === 'yes') {
+                  onCertDecide(
+                    alertDetails.data.id,
+                    alertDetails.data.data,
+                    alertDetails.data.type
+                  );
+                }
+              }}
             />
           </div>
+        </div>
+      )}
+      {errorMessage && (
+        <div className='fixed top-[0] right-[0] bottom-[0] left-[255px] flex justify-center items-center bg-[#918a8a61]'>
+          <div>
+            <ErrorIndicator errorMessage={pendingErrorMessage} />
+          </div>
+        </div>
+      )}
+      {isLoading && (
+        <div className='w-full h-full bg-transparent'>
+          <LoadingIndicator />
         </div>
       )}
     </div>
