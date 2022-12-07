32f8939ced7e2e6d20eb354e433ff2370268545b
SOG-web <raheemolalekanusman84@gmail.com>
2022-12-07 16:45:37 +0100

    url updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://edujobsng.herokuapp.com/api/v1/admin";
+const BASE_URL = "https://edujobsng.up.railway.app/api/v1/admin";
 
 export default axios.create({
   baseURL: BASE_URL,
